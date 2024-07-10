5ba6827b0b30316de5d1f5b08b9407cd1c486ebe
Merlin Barbou <merlin31770@gmail.com>
2024-07-10 14:47:37 +0200

    fix bug GET All

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -19,9 +19,32 @@ class ContentController extends AbstractController
     #[Route('/api/content', name: 'content', methods: ['GET'])]
     public function getContentList(ContentRepository $contentRepository, SerializerInterface $serializer): JsonResponse
     {
-        $contentList = $contentRepository->findAll();
-        $jsonContentList = $serializer->serialize($contentList, 'json');
-        return new JsonResponse($jsonContentList, Response::HTTP_OK, [], true);
+        $content = $contentRepository->findAll();
+        if ($content) {
+            // Normaliser le contenu pour éviter les références circulaires
+            $data = $serializer->normalize($content, null, [
+                AbstractObjectNormalizer::ATTRIBUTES => [
+                    'id',
+                    'titre',
+                    'categorie',
+                    'video',
+                    'Langue',
+                    'episodes' => [
+                        'id',
+                        'numero_episode',
+                        'titre_episode',
+                        'duree',
+                        'saison' => [
+                            'id',
+                            'numero_saison'
+                        ]
+                    ]
+                ],
+            ]);
+
+            return new JsonResponse($data, Response::HTTP_OK);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
     #[Route('/api/content/{id}', name: 'detailContent', methods: ['GET'])]
